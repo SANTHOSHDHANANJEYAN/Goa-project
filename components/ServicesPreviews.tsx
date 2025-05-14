7fe436e0ae17c4f29bfe99d834ae84df96c6e050
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-05-14 11:09:38 +0530

    1019

--- components/ServicesPreviews.tsx
+++ components/ServicesPreviews.tsx
@@ -27,7 +27,7 @@ const services = [
 
 export default function ServicesPreview() {
   return (
-    <section className="py-20 bg-transparent">
+    <section className="py-20 bg-transparent pb-[3rem]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map((service) => (
