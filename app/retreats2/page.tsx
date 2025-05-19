af776ebe694d00302107db26382c6215eaabe348
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-05-19 13:48:04 +0530

    1905

---
+++ app/retreats2/page.tsx
@@ -0,0 +1,16 @@
+import Experience7 from "@/components/R7days/Experience7";
+import HeroSection7 from "@/components/R7days/Herosection7";
+import Highlights7 from "@/components/R7days/Highlights7";
+import HighlightSlider7 from "@/components/R7days/HighlightSlider7";
+import PackagesSection7 from "@/components/R7days/PackagesSection7";
+import ProgramSection7 from "@/components/R7days/ProgramSection7";
+import RetreatOptions7 from "@/components/R7days/RetreatOptions7";
+import TestimonialVideos7 from "@/components/R7days/TestimonialVideos7";
+
+
+
+export default function Page() {
+
+  return (<><HeroSection7/><RetreatOptions7/><PackagesSection7/><ProgramSection7/><Experience7/><Highlights7/><HighlightSlider7/><TestimonialVideos7/></>
+  ) 
+} 
