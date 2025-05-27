178ece4fd1557393ba7fbf14477c4d0a881a55a8
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-05-27 15:54:07 +0530

    2705

--- components/Appform.tsx
+++ components/Appform.tsx
@@ -46,7 +46,7 @@ export default function ContactForm() {
         </div>
 
         {/* Right Section - Form */}
-        <div className="md:w-1/2 w-full bg-white shadow-lg rounded-xl p-8">
+        <div className="md:w-1/2 w-full  shadow-lg rounded-xl p-8">
           <form className="flex flex-col gap-4">
             <input
               type="text"
