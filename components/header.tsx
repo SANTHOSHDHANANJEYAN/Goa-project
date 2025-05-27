eb1dbde09d57b97944f72f1c99917003a744ec01
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-05-27 11:34:00 +0530

    2705

--- components/header.tsx
+++ components/header.tsx
@@ -119,7 +119,7 @@ export default function Header() {
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-white backdrop-blur-md shadow-sm py-3 transition-colors">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-[90rem] px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-3 items-center h-16">
           <div className="flex items-center gap-6 ">
             <Button variant="ghost" size="icon" className="hover:bg-transparent focus:bg-transparent active:bg-transparent" onClick={() => setMobileMenuOpen(true)}>
