d5534bc58a3870f1e33fd7e6bb013951447588a4
SANTHOSHDHANANJEYAN <116083112+SANTHOSHDHANANJEYAN@users.noreply.github.com>
2025-05-29 16:57:20 +0530

    Update page.tsx

--- app/50/page.tsx
+++ app/50/page.tsx
@@ -88,12 +88,11 @@ const FiftyHourTTPage = () => {
   {/* <AnimateButton/> */}
   <Schedule/>
 
-<section className="mt-20 px-4 sm:px-6 lg:px-8">
-<h2 className="text-lg sm:text-xl md:text-2xl lg:text-[22px] font-bold text-center mb-4 text-[#150e70]">
-  Transformational teachings of Hatha Yoga, Vinyasa Yoga and Ashtanga Yoga to give<br className="hidden sm:inline" />
-  our students an extensive and comprehensive teaching experience
-</h2>
-  <h2 className="text-xs sm:text-sm md:text-base lg:text-[15px] font-bold text-center mb-8 text-[#150e70]">
+ <section className="mt-20 px-4 sm:px-6 lg:px-8">
+  <h2 className="text-lg sm:text-xl md:text-2xl lg:text-[22px] font-bold text-center mb-4 text-[#150e70]">
+    Transformational teachings of Hatha Yoga, Vinyasa Yoga and Ashtanga Yoga to give our students an extensive and comprehensive teaching experience
+  </h2>
+  <h2 className="text-xs sm:text-sm md:text-base lg:text-[10px] font-bold text-center mb-8 text-[#150e70]">
     Education experience covering all key aspects of the ancient practice and philosophy of yoga
   </h2>
 
