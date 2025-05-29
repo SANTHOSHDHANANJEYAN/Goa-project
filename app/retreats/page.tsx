818c5b59f85b27c3e3b52342346693dd605be4cc
SANTHOSHDHANANJEYAN <116083112+SANTHOSHDHANANJEYAN@users.noreply.github.com>
2025-05-29 17:17:01 +0530

    Update page.tsx

--- app/retreats/page.tsx
+++ app/retreats/page.tsx
@@ -17,7 +17,6 @@ import TwoListing7 from "@/components/R7days/TwoListing7";
 import DinningSlides7 from "@/components/R7days/DinningSlides7";
 import RoomSlides7 from "@/components/R7days/RoomSlides7";
 import Faqs7 from "@/components/R7days/Faqs7";
-import About7 from "@/components/R7days/About7";
 import About71 from "@/components/R7days/About71";
 
 
@@ -26,7 +25,6 @@ export default function Page() {
 
   return (<>
   <SpaHero7/>
-  <About7/>
   <About71/>
   <KeySkills7/>
   <TwoListing7/>
