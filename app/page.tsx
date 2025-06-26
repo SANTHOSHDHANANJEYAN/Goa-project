e2bb927c666a94e99029673ab897bf497f663216
SANTHOSHDHANANJEYAN <santhoshd318@gmail.com>
2025-06-26 15:42:45 +0530

    1445

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
             strokeLinecap="round"
             strokeLinejoin="round"
           >
-            <path d="M14 5l7 7m0 0l-7 7m7-7H3" />
+            <path d="/Aboutyogshala#InGoa" />
           </svg>
           Learn More
         </Button>
