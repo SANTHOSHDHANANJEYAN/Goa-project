7c0cfec7ba62bdf0e6785ce58682525c30fbcc2e
SANTHOSHDHANANJEYAN <116083112+SANTHOSHDHANANJEYAN@users.noreply.github.com>
2025-10-14 23:12:39 +0530

    Update AboutTraining50.tsx

--- components/50hr/AboutTraining50.tsx
+++ components/50hr/AboutTraining50.tsx
@@ -318,7 +318,7 @@ export default function AboutTraining() {
           >
             {/* Portrait Thumbnail — show actual image size */}
             <img
-              src="/aboutpics/Rishikul Yogshala Goa 1.jpg"
+              src="/aboutpics/about50.jpg"
               alt="Instagram Reel: 50 Hour Yoga Teacher Training in Goa"
               className="max-w-full h-auto object-contain transition-transform duration-500 group-hover:scale-105"
             />
